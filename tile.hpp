f2971771e677c6763fece64c7260d6ea0934c46e
RehanFastians <rehan14rs@gmail.com>
2025-12-13 21:39:15 +0500

    finalizing

--- tile.hpp
+++ tile.hpp
@@ -14,11 +14,11 @@ public:
     void draw(int y, int x, int tileSize);       // Draw the image on (X,Y) position
     bool isPossible(Tile &other, int direction); // Direction specify Tile other is in which direction
 
-    Tile(const Tile &) = delete; // disable copy
-    Tile &operator=(const Tile &) = delete;
+    // Tile(const Tile &) = delete; // disable copy
+    // Tile &operator=(const Tile &) = delete;
 
-    Tile(Tile &&) = default; // allow move
-    Tile &operator=(Tile &&) = default;
+    // Tile(Tile &&) = default; // allow move
+    // Tile &operator=(Tile &&) = default;
     ~Tile();
 
     friend class Grid;
